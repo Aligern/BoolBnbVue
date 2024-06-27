88cb68e61c853198cdcdc2def216715933b113e3
Aligern <sasso.luca93@gmail.com>
2024-06-27 17:41:10 +0200

    Co-authored-by: gelko@hotmail.it <gelko@hotmail.it>
    add base scaffolding, add headerComponent, addFooterComponent

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
-  <div>
-    <h1>
-      ciao
-    </h1>
-  </div>
+  <HeaderComponent />
+  <main>
+    <h1>ciao</h1>
+  </main>
+  <FooterComponent />
+  
 </template>
 
 <script>
+import axios from 'axios';
+import { store } from './store';
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
   export default {
-    name: 'App'
+    name: 'App',
+    components: { 
+      HeaderComponent,
+      FooterComponent,
+      },
+      data() {
+        return {
+          store
+        }
+      },
+      methods:{
+      
+      }
   }
 </script>
 
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="container">
+        <nav class="d-flex justify-content-between">
+            <div>logo</div>
+            <div>menu</div>
+        </nav>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponent',
+    }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
