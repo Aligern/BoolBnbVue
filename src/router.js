19d17eaf4d718af4305714f40b05ea5cf69204be
AngeloC89 <Angelociulla89@gmail.com>
2024-06-28 16:36:52 +0200

    create the routes for the pages

--- src/router.js
+++ src/router.js
@@ -1,9 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router";
-
+import AppHome from "./pages/AppHome.vue";
+import Details from "./pages/Details.vue";
+import NotFound from "./pages/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    {
+      path: "/",
+      name: "home",
+      component: AppHome, 
+    },
+    {
+      path: "/details",
+      name: "details",
+      component: Details, 
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "not-found",
+      component: NotFound, 
+    },
     
    
   ],
