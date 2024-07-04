f47db2196a7fd187b24ded3fc8ca26ef27a7922b
AngeloC89 <Angelociulla89@gmail.com>
2024-07-04 12:43:38 +0200

    made canvas component

--- src/components/CanvasComponent.vue
+++ src/components/CanvasComponent.vue
@@ -1,18 +1,31 @@
 <template>
+  <button
+    class="btn btn-outline-dark"
+    type="button"
+    data-bs-toggle="offcanvas"
+    data-bs-target="#offcanvasRight"
+    aria-controls="offcanvasRight"
+  >
+    <i class="fa-solid fa-bars"></i>
+  </button>
 
-  <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Toggle right offcanvas</button>
-
-<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
-    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  <div
+    class="offcanvas offcanvas-end"
+    tabindex="-1"
+    id="offcanvasRight"
+    aria-labelledby="offcanvasRightLabel"
+  >
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
+      <button
+        type="button"
+        class="btn-close"
+        data-bs-dismiss="offcanvas"
+        aria-label="Close"
+      ></button>
+    </div>
+    <div class="offcanvas-body">...</div>
   </div>
-  <div class="offcanvas-body">
-    ...
-  </div>
-</div>
-
-
 </template>
 
 <script>
@@ -22,16 +35,14 @@
     data() {
       return {
         store,
-        
+
       }
     },
-   
+ 
+
 
 
   }
 </script>
 
-<style lang="scss" scoped>
-
-
-</style>
+<style lang="scss" scoped></style>
